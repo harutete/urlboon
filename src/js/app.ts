f6564c6463a9e6facc5656c5ef1c44173e5aa133
harutete <ai.takeuchi.10@gmail.com>
2020-02-10 23:52:17 +0900

    細かな表記ゆれを修正

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,12 +1,12 @@
-import '../sass/style.scss';
+import '../sass/style.scss'
 
 class JumpUrl {
   private readonly win: any
   private readonly doc: any
   private readonly setAttr: { [key: string]: string }
-  constructor () {
-    this.win = window;
-    this.doc = this.win.document;
+  constructor() {
+    this.win = window
+    this.doc = this.win.document
     this.setAttr = {
       wrap: 'jumpurl-wrap',
       textarea: 'box-url',
@@ -20,67 +20,68 @@ class JumpUrl {
     this.setTogglePanel()
     this.getUrlPage()
   }
-  private setPanel (): void {
+  private setPanel(): void {
     const body = this.doc.body
-    const wrap = this.doc.createElement('div');
-    const textarea = this.doc.createElement('textarea');
-    const modalButton = this.doc.createElement('button');
-    const getUrlButton = this.doc.createElement('button');
-    wrap.classList.add(this.setAttr.wrap);
-    wrap.appendChild(textarea);
-    wrap.appendChild(modalButton);
-    wrap.appendChild(getUrlButton);
-    textarea.setAttribute('id', this.setAttr.textarea);
-    modalButton.setAttribute('type', 'button');
-    modalButton.classList.add(this.setAttr.modalButton);
-    getUrlButton.setAttribute('type', 'button');
-    getUrlButton.classList.add(this.setAttr.getUrlButton);
-    getUrlButton.insertAdjacentHTML('beforeend', 'GET!!');
-    body.insertAdjacentElement('beforeend', wrap);
+    const wrap = this.doc.createElement('div')
+    const textarea = this.doc.createElement('textarea')
+    const modalButton = this.doc.createElement('button')
+    const getUrlButton = this.doc.createElement('button')
+    wrap.classList.add(this.setAttr.wrap)
+    wrap.appendChild(textarea)
+    wrap.appendChild(modalButton)
+    wrap.appendChild(getUrlButton)
+    textarea.setAttribute('id', this.setAttr.textarea)
+    modalButton.setAttribute('type', 'button')
+    modalButton.classList.add(this.setAttr.modalButton)
+    getUrlButton.setAttribute('type', 'button')
+    getUrlButton.classList.add(this.setAttr.getUrlButton)
+    getUrlButton.insertAdjacentHTML('beforeend', 'GET!!')
+    body.insertAdjacentElement('beforeend', wrap)
   }
-  private setTogglePanel (): void {
-    const button = this.doc.querySelector(`.${this.setAttr.modalButton}`);
-    const wrap = this.doc.querySelector(`.${this.setAttr.wrap}`);
+  private setTogglePanel(): void {
+    const button = this.doc.querySelector(`.${this.setAttr.modalButton}`)
+    const wrap = this.doc.querySelector(`.${this.setAttr.wrap}`)
     let togglePanel = () => {
       if (!wrap.classList.contains(this.setAttr.active)) {
-        wrap.classList.add(this.setAttr.active);
+        wrap.classList.add(this.setAttr.active)
       } else {
-        wrap.classList.remove(this.setAttr.active);
+        wrap.classList.remove(this.setAttr.active)
       }
-    };
+    }
 
-    button.addEventListener('click', togglePanel, false);
+    button.addEventListener('click', togglePanel, false)
   }
-  private getUrlPage  (): void {
-    let textVal;
-    const box = this.doc.getElementById(this.setAttr.textarea);
-    const searchStr = new RegExp('http(s)://', 'ig');
+  private getUrlPage(): void {
+    let textVal
+    const box = this.doc.getElementById(this.setAttr.textarea)
+    const searchStr = new RegExp('http(s)://', 'ig')
     const accessPage = (): void => {
-      let urlArr;
-      let i;
-      let len;
-      textVal = box.value;
+      let urlArr
+      let i
+      let len
+      textVal = box.value
 
       if (textVal === '') {
-        this.win.alert('URLを入力してください');
-        return;
+        this.win.alert('URLを入力してください')
+
+        return
       }
 
-      urlArr = textVal.split(/\r\n|\n/);
-      len = urlArr.length;
+      urlArr = textVal.split(/\r\n|\n/)
+      len = urlArr.length
       for (i = 0; i < len; i++) {
         if (searchStr.test(urlArr[i])) {
-          this.win.open(urlArr[i]);
+          this.win.open(urlArr[i])
         } else {
-          continue;
+          continue
         }
 
-        searchStr.lastIndex = 0;
+        searchStr.lastIndex = 0
       }
-    };
+    }
 
-    this.doc.querySelector(`.${this.setAttr.getUrlButton}`).addEventListener('click', accessPage, false);
+    this.doc.querySelector(`.${this.setAttr.getUrlButton}`).addEventListener('click', accessPage, false)
   }
 }
 
-new JumpUrl().init();
+new JumpUrl().init()
