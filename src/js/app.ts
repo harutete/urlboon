46b3139437dc3777194aa7ea18c7277b317964a3
harutete <ai.takeuchi.10@gmail.com>
2020-02-10 23:35:06 +0900

    型修正

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,97 +1,95 @@
 import '../sass/style.scss';
 
 interface configOptions {
-    setElm: any,
-    SetAttr: any
+  setElm: { [key: string]: HTMLBodyElement },
+  setAttr: { [key: string]: string }
 }
-class jumpUrl {
-    win: any
-    doc: any
-    config: any
-    constructor () {
-        this.win = window;
-        this.doc = this.win.document;
-        this.config = {
-            setElm: {
-                body: this.doc.body
-            },
-            setAttr: {
-                wrap: 'jumpurl-wrap',
-                textarea: 'box-url',
-                modalButton: 'btn-modal',
-                getUrlButton: 'btn-geturl',
-                active: 'active'
-            }
+
+class JumpUrl {
+  private readonly win: any
+  private readonly doc: any
+  private readonly config: configOptions
+  constructor () {
+    this.win = window;
+    this.doc = this.win.document;
+    this.config = {
+      setElm: {
+        body: this.doc.body
+      },
+      setAttr: {
+        wrap: 'jumpurl-wrap',
+        textarea: 'box-url',
+        modalButton: 'btn-modal',
+        getUrlButton: 'btn-geturl',
+        active: 'active'
+      }
+    }
+  }
+  public init(): void {
+    this.setPanel()
+    this.setTogglePanel()
+    this.getUrlPage()
+  }
+  private setPanel (): void {
+    const wrap = this.doc.createElement('div');
+    const textarea = this.doc.createElement('textarea');
+    const modalButton = this.doc.createElement('button');
+    const getUrlButton = this.doc.createElement('button');
+    wrap.classList.add(this.config.setAttr.wrap);
+    wrap.appendChild(textarea);
+    wrap.appendChild(modalButton);
+    wrap.appendChild(getUrlButton);
+    textarea.setAttribute('id', this.config.setAttr.textarea);
+    modalButton.setAttribute('type', 'button');
+    modalButton.classList.add(this.config.setAttr.modalButton);
+    getUrlButton.setAttribute('type', 'button');
+    getUrlButton.classList.add(this.config.setAttr.getUrlButton);
+    getUrlButton.insertAdjacentHTML('beforeend', 'GET!!');
+    this.config.setElm.body.insertAdjacentElement('beforeend', wrap);
+  }
+  private setTogglePanel (): void {
+    const button = this.doc.querySelector(`.${this.config.setAttr.modalButton}`);
+    const wrap = this.doc.querySelector(`.${this.config.setAttr.wrap}`);
+    let togglePanel = () => {
+      if (!wrap.classList.contains(this.config.setAttr.active)) {
+        wrap.classList.add(this.config.setAttr.active);
+      } else {
+        wrap.classList.remove(this.config.setAttr.active);
+      }
+    };
+
+    button.addEventListener('click', togglePanel, false);
+  }
+  private getUrlPage  (): void {
+    let textVal;
+    const box = this.doc.getElementById(this.config.setAttr.textarea);
+    const searchStr = new RegExp('http(s)://', 'ig');
+    const accessPage = (): void => {
+      let urlArr;
+      let i;
+      let len;
+      textVal = box.value;
+
+      if (textVal === '') {
+        this.win.alert('URLを入力してください');
+        return;
+      }
+
+      urlArr = textVal.split(/\r\n|\n/);
+      len = urlArr.length;
+      for (i = 0; i < len; i++) {
+        if (searchStr.test(urlArr[i])) {
+          this.win.open(urlArr[i]);
+        } else {
+          continue;
         }
 
-        this.setPanel()
-        this.setTogglePanel()
-        this.getUrlPage()
-    }
-    setPanel (): void {
-        const wrap = this.doc.createElement('div');
-        const textarea = this.doc.createElement('textarea');
-        const modalButton = this.doc.createElement('button');
-        const getUrlButton = this.doc.createElement('button');
+        searchStr.lastIndex = 0;
+      }
+    };
 
-        wrap.classList.add(this.config.setAttr.wrap);
-        wrap.appendChild(textarea);
-        wrap.appendChild(modalButton);
-        wrap.appendChild(getUrlButton);
-        textarea.setAttribute('id', this.config.setAttr.textarea);
-        modalButton.setAttribute('type', 'button');
-        modalButton.classList.add(this.config.setAttr.modalButton);
-        getUrlButton.setAttribute('type', 'button');
-        getUrlButton.classList.add(this.config.setAttr.getUrlButton);
-        getUrlButton.insertAdjacentHTML('beforeend', 'GET!!');
-
-        this.config.setElm.body.insertAdjacentElement('beforeend', wrap);
-    }
-    setTogglePanel (): void {
-        const button = this.doc.querySelector(`.${this.config.setAttr.modalButton}`);
-        const wrap = this.doc.querySelector(`.${this.config.setAttr.wrap}`);
-        let togglePanel = () => {
-            if (!wrap.classList.contains(this.config.setAttr.active)) {
-                wrap.classList.add(this.config.setAttr.active);
-            } else {
-                wrap.classList.remove(this.config.setAttr.active);
-            }
-        };
-
-        button.addEventListener('click', togglePanel, false);
-    }
-    getUrlPage  (): void {
-        let textVal;
-        const box = this.doc.getElementById(this.config.setAttr.textarea);
-        const searchStr = new RegExp('http(s)://', 'ig');
-        const accessPage = () => {
-            let urlArr;
-            let i;
-            let len;
-
-            textVal = box.value;
-
-            if (textVal === '') {
-                this.win.alert('URLを入力してください');
-
-                return;
-            }
-
-            urlArr = textVal.split(/\r\n|\n/);
-            len = urlArr.length;
-
-            for (i = 0; i < len; i++) {
-                if (searchStr.test(urlArr[i])) {
-                    this.win.open(urlArr[i]);
-                } else {
-                    continue;
-                }
-
-                searchStr.lastIndex = 0;
-            }
-        };
-        this.doc.querySelector(`.${this.config.setAttr.getUrlButton}`).addEventListener('click', accessPage, false);
-    }
+    this.doc.querySelector(`.${this.config.setAttr.getUrlButton}`).addEventListener('click', accessPage, false);
+  }
 }
 
-new jumpUrl();
+new JumpUrl().init();
