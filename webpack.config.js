5fe7af2062b9748cb15eb01625dbd56c4fadcce7
harutete <ai.takeuchi.10@gmail.com>
2020-06-29 00:46:54 +0900

    インデント調整

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,51 @@ const path = require('path');
 const MODE = 'development';
 
 const config = {
-    mode: MODE,
-    entry: './src/js/app.ts',
-    output: {
-        path: path.join(__dirname, 'dist/js/'),
-        filename: 'master.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist/'),
-        open: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-              test: /\.ts$/,
-              loader: 'ts-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                            importLoaders: 2
-                        }
-                    },
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        '@babel/preset-env'
-                    ]
-                }
+  mode: MODE,
+  entry: './src/js/app.ts',
+  output: {
+    path: path.join(__dirname, 'dist/js/'),
+    filename: 'master.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/'),
+    open: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 2
             }
+          },
+          'sass-loader'
         ]
-    }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
+      }
+    ]
+  }
 }
 
 module.exports = config;
